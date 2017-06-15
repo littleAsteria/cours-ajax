91642b94e0a659a4757935bdee5d1e55edebf2c7
littleAsteria <cinnamonpan@gmail.com>
2017-06-15 17:26:59 +0200

    exercice2

--- scripts/script.js
+++ scripts/script.js
@@ -52,24 +52,88 @@ $(function(){//lance le code seulement lorsque le html est pret
 		request.done(function(data) {// ce qui se trouve entre les parenthéses de la fonction c'est ce que nous renvoie le serveur (ici le tableau des utilisateurs peu importe le nom )
 					
 					let content="";// On créé un contenu vide auquel on ajoutera des valeurs (conf.boutique-php)
-					
+
 					data.forEach(function(element){//parcourt toutes les cases des tableaux
 					//ce qui se trouve entre les parenthéses de la fonction correspond aux elements récupérés dans le tableau
-						content +='<li><a href="#">' + element.name +'</a><li>';
+						content +='<li id="Mike'+ element.id+'"><a href="#">' + element.name +'</a><li>';
 						//on ne veut que les noms (on associe element à l'indice nom du tableau pour ne récupérer que les noms)
+						//on ajoute un id à li pour la suite de l'exercice
 					});
 					//on efface le code html précédent car il pourrait avoir plus d'utilisateur que de li
 					//A la place on ajoute au "content" une nouvelle valeur correspondand aux noms des utilisateurs
 
-					$("#right_column ul").html(content);//balise aside regroupant les li, on remplace le contenu  par le contenu du content
-				
+					$("#right_column ul").html(content);
+					//balise aside regroupant les li, on remplace le contenu  par le contenu du content
+//--------------------------------------------------------Exercice 2-----------------------------------------------------------
+
+					$("#right_column ul > li ").click(function(e){
+					//si je clique sur une balise li
+						
+						e.preventDefault();
+
+						/*recupération de l'id : */
+						let idUser = $(this).attr("id");// on change l'id mike en idUser
+						console.log(idUser.split("-"));//on ajoute un tiret pour chaque id User-3 = array("User","3") 
+						//split coupe par rapport au caractére '-' 
+						idUser= idUser.split("-");
+
+							let ficheUser = $.ajax({
+								url:"https://jsonplaceholder.typicode.com/users",
+								method: "GET",
+								data: {id : idUser[1]},
+								dataType : "json"//option
+							});
+
+						ficheUser.done(function(dataUser){
+
+							console.info(dataUser[0].username+" "+dataUser[0].email);
+
+						});
+
+					});
+					
+
+
+					/*$('#right_column ul li').on('click', function(e){
+
+						e.stopPropagation();
+
+							let request2 = $.ajax({
+
+								url:'https://jsonplaceholder.typicode.com/users',
+								method:'GET',
+								dataType:'json'
+
+						});
+
+
+
+					});*/
+/*
+					request2.done(function(d){
+
+		 				d.forEach(function(element){
+
+		 				console.info(element.username) + '<br>' + console.info(element.email);
+
+		 			});
+
+		});*/
+
+
+
 		});
  
 		request.fail(function( jqXHR, textStatus ) {
   				alert( "Request failed: " + textStatus );
 		});
 
+//-------------------------------Console info----------------------------------
+// faire un console.info de username et email de chaque utilisateur lorsqu'on clique sur un li
 		
+
+		
+
+
 	
-		
 });
